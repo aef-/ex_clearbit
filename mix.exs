b89bd55fdf893e6f1f88a15b4af82ab6707e1c74
Keith Mattix II <keithmattix2@gmail.com>
2020-02-20 08:50:31 -0600

    Bump version (0.2.0)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExClearbit.Mixfile do
 
   def project do
     [app: :ex_clearbit,
-     version: "0.1.3",
+     version: "0.2.0",
      name: "ExClearbit",
      elixir: "~> 1.9.2",
      description: description(),
